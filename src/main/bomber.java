80f0b0a58866ad4e991a9b9707ea7893545419d6
= <=>
2021-12-01 22:19:05 +0700

    update

--- src/main/bomber.java
+++ src/main/bomber.java
@@ -3,17 +3,6 @@ package main;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author afkda
- */
-
 public class keyboardController implements KeyListener {
     public boolean keyUp, keyDown, keyLeft, keyRight;
     @Override
